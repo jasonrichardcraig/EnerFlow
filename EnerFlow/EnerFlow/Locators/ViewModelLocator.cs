9f1652746c8af6169851c7ad98d860632ebf5e38
Jason Craig <jason.craig@outlook.com>
2024-10-28 00:53:09 -0600

    Changed Project Properties and icons

--- EnerFlow/EnerFlow/Locators/ViewModelLocator.cs
+++ EnerFlow/EnerFlow/Locators/ViewModelLocator.cs
@@ -5,6 +5,8 @@ namespace EnerFlow.Locators
 {
     public class ViewModelLocator
     {
+#pragma warning disable CA1822 // Mark members as static (false positive), this property is late time bound
         public MainViewModel MainViewModel => Ioc.Default.GetService<MainViewModel>() ?? null!;
+#pragma warning restore CA1822 // Mark members as static
     }
 }
